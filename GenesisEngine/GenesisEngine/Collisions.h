7c17aa62b5a7960f5a7c29e7d07e232955e80521
bfelch <kagrenac@live.com>
2015-04-01 12:05:43 -0500

    detection templates

--- GenesisEngine/GenesisEngine/Collisions.h
+++ GenesisEngine/GenesisEngine/Collisions.h
@@ -1,8 +1,16 @@
 #pragma once
+
+#include "Vector2.h"
+
 class Collisions
 {
 public:
 	Collisions();
 	~Collisions();
+
+	static bool hasCollision(Rect2D, Rect2D);
+	static bool hasCollision(Rect2D, Circ2D);
+	static bool hasCollision(Circ2D, Rect2D);
+	static bool hasCollision(Circ2D, Circ2D);
 };
 
