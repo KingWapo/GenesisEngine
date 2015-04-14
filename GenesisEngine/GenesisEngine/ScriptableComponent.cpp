c8f3fa2a0b8e5e6c691c8c0ec8a9a60c9f03f0f1
KingWapo <Holcombea9172@my.uwstout.edu>
2015-04-14 13:41:30 -0500

    Beginning of Scripting Component

--- GenesisEngine/GenesisEngine/ScriptableComponent.cpp
+++ GenesisEngine/GenesisEngine/ScriptableComponent.cpp
@@ -4,8 +4,15 @@ const char *ScriptableComponent::g_Name = "ScriptableComponent";
 
 ScriptableComponent::ScriptableComponent()
 {
+	m_name = NULL;
+	m_pDomain = mono_jit_init("script.exe");
+	m_pAssembly = mono_domain_assembly_open(m_pDomain, "script.exe");
 }
 
+ScriptableComponent::ScriptableComponent(const char* p_name)
+{
+	m_name = p_name;
+}
 
 ScriptableComponent::~ScriptableComponent()
 {
