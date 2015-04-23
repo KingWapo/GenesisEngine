5675473573c6e918c1f420b32285e00c11563a4c
KingWapo <Holcombea9172@my.uwstout.edu>
2015-04-23 15:27:13 -0500

    Fixes?
    :jeans: :jeans: :jeans: :jeans: :jeans: :jeans: :jeans: :jeans: :jeans:
    :jeans: :jeans: :jeans: :jeans: :jeans: :jeans: :jeans:

--- GenesisEngine/ScriptEngineInterface/Program.cs
+++ GenesisEngine/ScriptEngineInterface/Program.cs
@@ -3,10 +3,37 @@ using Mono.CSharp;
 
 namespace ScriptEngineInterface
 {
+    public interface IStringModifier { string modify(string s); }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Evaluator myEval = new Evaluator(
+                new CompilerContext(new CompilerSettings(), new ConsoleReportPrinter()));
+
+            myEval.ReferenceAssembly(typeof(IStringModifier).Assembly);
+            myEval.Compile(
+                @"
+                 public class Upper : ScriptEngineInterface.IStringModifier
+                {
+                    public string modify(string s) { return s.ToUpper(); }
+                }");
+
+            Console.WriteLine("Mini Console\n");
+
+            while (true)
+            {
+                Console.Write(">");
+                string line = Console.ReadLine();
+                if (line == null || line == "") break;
+
+                object result;
+                bool result_set;
+                myEval.Evaluate(line, out result, out result_set);
+
+                if (result_set) Console.WriteLine(result);
+            }
         }
     }
 }
